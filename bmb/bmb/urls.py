2d33252b46c92dd4b42c49bc6109b913e421dc2d
CBergane <christian.bergane@gmail.com>
2023-08-09 23:04:51 +0000

    added cart functionalety

--- bmb/bmb/urls.py
+++ bmb/bmb/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth import views
 from django.conf.urls.static import static
 from django.urls import path
 
-from cart.views import add_to_cart
+from cart.views import add_to_cart, cart, checkout
 from core.views import frontpage, shop, signup
 from products.views import produkt
 
@@ -17,4 +17,6 @@ urlpatterns = [
     path('shop/', shop, name='shop'),
     path('shop/<slug:slug>/', produkt, name='produkt'),
     path('add_to_cart/<int:produkt_id>/', add_to_cart, name='add_to_cart'),
+    path('cart/', cart, name='cart'),
+    path('cart/checkout/', checkout, name='checkout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- bmb/cart/views.py
+++ bmb/cart/views.py
@@ -6,3 +6,9 @@ def add_to_cart(request, produkt_id):
     cart.add(produkt_id)
 
     return render(request, 'cart/menu_cart.html')
+
+def cart(request):
+    return render(request, 'cart/cart.html')
+
+def checkout(request):
+    return render(request, 'cart/checkout.html')
