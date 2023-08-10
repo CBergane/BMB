c4622cbd3c8a88fc7a614fcd6bf58dc7339c1b96
CBergane <christian.bergane@gmail.com>
2023-08-10 08:34:33 +0000

    changes urls and added custom title to all pages

--- bmb/bmb/urls.py
+++ bmb/bmb/urls.py
@@ -1,22 +1,13 @@
 from django.conf import settings
 from django.contrib import admin
-from django.contrib.auth import views
-from django.conf.urls.static import static
-from django.urls import path
 
-from cart.views import add_to_cart, cart, checkout
-from core.views import frontpage, shop, signup
-from products.views import produkt
+from django.conf.urls.static import static
+from django.urls import path, include
+
 
 urlpatterns = [
+    path('', include('core.urls')),
+    path('cart/', include('cart.urls')),
     path('admin/', admin.site.urls),
-    path('', frontpage, name='frontpage'),
-    path('signup/', signup, name='signup'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
-    path('login/', views.LoginView.as_view(template_name='core/login.html'), name='login'),
-    path('shop/', shop, name='shop'),
-    path('shop/<slug:slug>/', produkt, name='produkt'),
-    path('add_to_cart/<int:produkt_id>/', add_to_cart, name='add_to_cart'),
-    path('cart/', cart, name='cart'),
-    path('cart/checkout/', checkout, name='checkout'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
