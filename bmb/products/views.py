daf34f61f1d4eabeb7db3b7e9553eb827c5b0680
CBergane <christian.bergane@gmail.com>
2023-08-14 21:00:30 +0000

    fixed rating models

--- bmb/products/views.py
+++ bmb/products/views.py
@@ -1,10 +1,32 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
-from .models import Produkt
+from .models import Produkt, Review
 
 
 def produkt(request, slug):
 
     produkt = get_object_or_404(Produkt, slug=slug)
+
+    if request.method == 'POST':
+        rating = request.POST.get('rating', 3)
+        content = request.POST.get('content', '')
+
+        if content:
+            reviews = Review.objects.filter(created_by=request.user, product=produkt)
+
+            if reviews.count() > 0:
+                review = reviews.first()
+                review.rating = rating
+                review.content = content
+                review.save()
+            else:
+                review = Review.objects.create(
+                    product=product,
+                    rating=rating,
+                    content=content,
+                    created_by=request.user
+                )
+
+            return redirect('produkt', slug=slug)
 
     return render(request, 'products/product.html', {'produkt':produkt,})
