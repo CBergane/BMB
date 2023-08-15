89afd1fdd44d2d09a853ae49c4aaf693a3c7ceea
CBergane <christian.bergane@gmail.com>
2023-08-15 20:45:44 +0000

    added swish function and linked new database

--- bmb/bmb/settings.py
+++ bmb/bmb/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-cbergane-bmb-hwfowlqnoyb.ws-eu103.gitpod.io', 'https://bmb-annelie-e3fc68fd7d04.herokuapp.com', 'localhost', 'bmb-annelie-e3fc68fd7d04.herokuapp.com', ]
+ALLOWED_HOSTS = ['8000-cbergane-bmb-hwfowlqnoyb.ws-eu103.gitpod.io', 'localhost', 'bmb-annelie-e3fc68fd7d04.herokuapp.com', ]
 
 SITE_ID = 1
 
