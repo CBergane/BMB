0eb71ac3fc50b671bd9496d62c853b21c54acd49
CBergane <christian.bergane@gmail.com>
2024-01-20 16:36:32 +0000

    added some minor changes

--- bmb/products/admin.py
+++ bmb/products/admin.py
@@ -1,10 +1,20 @@
 from django.contrib import admin
+from django.db.models import Count
 from .models import Category, Produkt
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('namn', 'parent', 'slug')  # Visa namn, överordnad kategori och slug
-    list_filter = ('parent',)  # Filtrera baserat på överordnad kategori
+    list_display = ('namn', 'parent', 'slug', 'number_of_subcategories')
+    list_filter = ('parent',)
     search_fields = ('namn',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.annotate(subcat_count=Count('children')).order_by('-subcat_count')
+
+    def number_of_subcategories(self, obj):
+        return obj.subcat_count
+    number_of_subcategories.admin_order_field = 'subcat_count'
+    number_of_subcategories.short_description = 'Antal Underkategorier'
 
 class ProduktAdmin(admin.ModelAdmin):
     list_display = ('namn', 'category', 'is_fabric', 'is_stubbie', 'is_active', 'pris', 'skapad')
