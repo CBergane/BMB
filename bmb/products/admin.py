845b13a018050fff60baf5c81f615a587f46bcc9
CBergane <christian.bergane@gmail.com>
2024-08-22 08:27:52 +0000

    added som minor funktions and trubbelshotting

--- bmb/products/admin.py
+++ bmb/products/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
-from django.db.models import Count
+from django.db.models import Count, Q
 from .models import Category, Produkt, WashInstruction, Color, ProductVariant
 
+
+class ParentCategoryListFilter(admin.SimpleListFilter):
+    title = 'Parent Category'
+    parameter_name = 'is_parent'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('yes', 'Parent Categories'),
+            ('no', 'Non-Parent Categories'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'yes':
+            return queryset.filter(parent__isnull=True)
+        elif self.value() == 'no':
+            return queryset.filter(parent__isnull=False)
+        return queryset
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('namn', 'parent', 'slug', 'number_of_subcategories')
@@ -16,6 +33,12 @@ class CategoryAdmin(admin.ModelAdmin):
         return obj.subcat_count
     number_of_subcategories.admin_order_field = 'subcat_count'
     number_of_subcategories.short_description = 'Antal Underkategorier'
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'parent':
+            # Filter out categories that already have a parent
+            kwargs["queryset"] = Category.objects.filter(parent__isnull=True)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 
 @admin.register(WashInstruction)
@@ -33,11 +56,26 @@ class ProductVariantInline(admin.TabularInline):
 
 
 class ProduktAdmin(admin.ModelAdmin):
-    list_display = ('namn', 'category', 'is_fabric','slug', 'is_stubbie', 'is_active', 'pris', 'skapad')
+    list_display = ('namn', 'category', 'is_fabric', 'slug', 'is_stubbie', 'is_active', 'pris', 'skapad', 'number_of_images')
     list_filter = ('category', 'is_fabric', 'is_stubbie', 'is_active')
     search_fields = ('namn', 'beskrivning', 'blandning', 'kvalitet', 'färg', 'motiv')
     filter_horizontal = ('wash_instructions',)
     inlines = [ProductVariantInline]
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            # Filtrera bort kategorier som redan har en förälder
+            kwargs["queryset"] = Category.objects.filter(parent__isnull=False)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def number_of_images(self, obj):
+        """
+        Räknar antalet bilder associerade med produkten.
+        """
+        images = [obj.image, obj.image2, obj.image3, obj.image4]
+        return sum(1 for image in images if image)  # Räknar icke-tomma bilder
+
+    number_of_images.short_description = 'Antal Bilder'
 
 
 admin.site.register(Category, CategoryAdmin)
--- bmb/products/models.py
+++ bmb/products/models.py
@@ -110,6 +110,20 @@ class Produkt(models.Model):
         else:
             return None
 
+    def get_thumbnail_image3(self):
+        if self.image3:
+            thumbnail_url = cloudinary_url(self.image3.public_id, width=300, height=300, crop="fill")[0]
+            return thumbnail_url
+        else:
+            return None
+
+    def get_thumbnail_image4(self):
+        if self.image4:
+            thumbnail_url = cloudinary_url(self.image4.public_id, width=300, height=300, crop="fill")[0]
+            return thumbnail_url
+        else:
+            return None
+
     def get_rating(self):
         reviews_total = 0
 
